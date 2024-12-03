59b2f6ab6c94efbda083fe9eca2eb2e09bc47a48
SongOnWater <469776792@qq.com>
2024-12-03 18:29:57 +0800

    create a branch for comfyUI

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .nodes import SubtitleExtractorWrapper
+
+NODE_CLASS_MAPPINGS = {
+     "SubtitleExtractorWrapper": SubtitleExtractorWrapper,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+     "SubtitleExtractorWrapper": "SubtitleExtractorWrapper",
+}
+__all__ = ['NODE_CLASS_MAPPINGS','NODE_DISPLAY_NAME_MAPPINGS']
---
+++ nodes.py
@@ -0,0 +1,32 @@
+from .backend.main import SubtitleExtractor
+import os 
+import pysrt
+
+class SubtitleExtractorWrapper:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": { 
+                "video_path": ("STRING", {"default":"",}),
+                "subtitle_area": ("BBOX"),
+            },
+        }
+
+    RETURN_TYPES = ("STRING", "STRING")
+    RETURN_NAMES = ("srt_path", "srt")
+    FUNCTION = "wrapper_run"
+    OUTPUT_NODE = True
+    def wrapper_run(self,video_path,subtitle_area):
+            if subtitle_area is not None:
+                x, y, width, height = subtitle_area
+                subtitle_area = (  y, y+height,x, x+width)
+            se = SubtitleExtractor(video_path, subtitle_area)
+            # 开始提取字幕z
+            se.run() 
+            srt_file=os.path.join(os.path.splitext(self.video_path)[0] + '.srt')
+            subs = pysrt.open(srt_file, encoding='utf-8')
+            return (srt_file,subs.text)
+    
